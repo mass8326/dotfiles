7d0647b4920f4ad6b025ef692e83c64cfb725e05
Magnus Reyes <me@magnusreyes.com>
2024-06-03 13:08:57 -0400

    Add unocss lsp config to silence missing handler

---
+++ dot_config/nvim/lua/plugins/unocss.lua
@@ -0,0 +1,14 @@
+return {
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      servers = {
+        unocss = {
+          handlers = {
+            ["textDocument/documentHighlight"] = function() end,
+          },
+        },
+      },
+    },
+  },
+}
