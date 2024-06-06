c4c1fbf3469f31184957d94ea942b8af9e505ef6
Magnus Reyes <me@magnusreyes.com>
2024-06-06 09:21:46 -0400

    Add telescope notify keyboard shortcut

--- dot_config/nvim/lua/config/keymaps.lua
+++ dot_config/nvim/lua/config/keymaps.lua
@@ -2,6 +2,14 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 
+local wk = require("which-key")
+wk.register({
+  t = {
+    name = "telescope",
+    n = { "<cmd>Telescope notify<cr>", "Show notifications" },
+  },
+}, { prefix = "<leader>" })
+
 -- Tab to confirm completion
 local cmp = require("cmp")
 local config = cmp.get_config()
