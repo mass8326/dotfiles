b491600c0018bed32e09abcaa4b634dafe218107
Magnus Reyes <me@magnusreyes.com>
2024-06-12 13:46:50 -0400

    Change wezterm fonts

--- dot_wezterm.lua
+++ dot_wezterm.lua
@@ -2,6 +2,13 @@ local wezterm = require("wezterm")
 local config = wezterm.config_builder()
 
 config.use_resize_increments = true
+config.font = wezterm.font_with_fallback({
+	"DejaVuSansM Nerd Font",
+	"Droid Sans Mono",
+	"Consolas",
+	"Courier New",
+	"monospace",
+})
 
 local is_linux = function()
 	return wezterm.target_triple:find("linux") ~= nil
