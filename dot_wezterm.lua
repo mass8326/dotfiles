ee149dc6370a53f3d283aa68f775c9800b5e1c5d
Magnus Reyes <me@magnusreyes.com>
2024-06-12 12:16:03 -0400

    Change wezterm to use resize increments

--- dot_wezterm.lua
+++ dot_wezterm.lua
@@ -1,6 +1,8 @@
 local wezterm = require("wezterm")
 local config = wezterm.config_builder()
 
+config.use_resize_increments = true
+
 local is_linux = function()
 	return wezterm.target_triple:find("linux") ~= nil
 end
