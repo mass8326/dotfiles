1832353bb73366de5f9ea3a7d85cc2024fc718e6
Magnus Reyes <me@magnusreyes.com>
2024-06-13 15:02:19 -0400

    Fix neotree plugin setup and add omz plugins

--- dot_config/nvim/lua/plugins/neotree.lua
+++ dot_config/nvim/lua/plugins/neotree.lua
@@ -1,14 +1,18 @@
--- Single click file tree navigation
-require("neo-tree").setup({
-  window = {
-    mappings = {
-      ["<LeftRelease>"] = "toggle_node",
+return {
+  {
+    "nvim-neo-tree/neo-tree.nvim",
+    opts = {
+      window = {
+        mappings = {
+          ["<LeftRelease>"] = "toggle_node",
+        },
+      },
+      buffers = {
+        follow_current_file = {
+          enabled = true,
+          leave_dirs_open = false,
+        },
+      },
     },
   },
-  buffers = {
-    follow_current_file = {
-      enabled = true,
-      leave_dirs_open = false,
-    },
-  },
-})
+}
