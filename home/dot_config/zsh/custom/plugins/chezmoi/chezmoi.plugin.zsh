785a7e87c74bb085268479dddb02091ae1e058ac
Magnus Reyes <me@magnusreyes.com>
2024-06-22 16:22:33 -0400

    Change chezmoi plugin to copy existing git aliases

--- home/dot_config/zsh/custom/plugins/chezmoi/chezmoi.plugin.zsh
+++ home/dot_config/zsh/custom/plugins/chezmoi/chezmoi.plugin.zsh
@@ -1,15 +1,12 @@
 [[ $+commands[chezmoi] ]] || return 0
 
 source <(chezmoi completion zsh)
-
 alias ch="chezmoi"
 alias che="chezmoi edit"
 alias chd="chezmoi diff"
 alias cha="chezmoi apply --verbose"
 alias chea="chezmoi edit --apply --verbose"
 alias chcd="chezmoi cd"
-alias chgst="chezmoi git status"
-alias chga="chezmoi git add"
-alias chgaa="chezmoi git add --all"
-alias chgc="chezmoi git commit"
-alias chgp="chezmoi git push"
+
+[[ $+commands[git] ]] || return 0
+source <(alias | awk -F "='" -f "${0:h}/alias.awk")
