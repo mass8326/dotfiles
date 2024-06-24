a3c8d6a846d0e958468ed1abd4c65e3f78ba1288
Magnus Reyes <me@magnusreyes.com>
2024-06-24 14:07:59 -0400

    Make moving buffers more convenient

---
+++ home/dot_config/nvim/lua/plugins/bufferline.lua
@@ -0,0 +1,9 @@
+return {
+  "akinsho/bufferline.nvim",
+  keys = {
+    { "[b", "<cmd>BufferLineMovePrev<cr>", desc = "Move Buffer Left" },
+    { "]b", "<cmd>BufferLineMoveNext<cr>", desc = "Move Buffer Right" },
+    { "[B", vim.NIL },
+    { "]B", vim.NIL },
+  },
+}
