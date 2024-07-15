1c3866ec3460961b3074b4dff578586737b58803
Magnus Reyes <me@magnusreyes.com>
2024-07-15 08:13:18 -0400

    Remove unnecessary bufferline key mapings

--- home/dot_config/nvim/exact_lua/exact_ui/bufferline.lua
+++ home/dot_config/nvim/exact_lua/exact_ui/bufferline.lua
@@ -1,16 +1,14 @@
 return {
-  "akinsho/bufferline.nvim",
-  opts = function(_, opts)
-    local bufferline = require("bufferline")
-    opts.options.style_preset = {
-      bufferline.style_preset_no_bold,
-      bufferline.style_preset_no_italic,
-    }
-  end,
-  keys = {
-    { "[b", "<cmd>BufferLineMovePrev<cr>", desc = "Move Buffer Left" },
-    { "]b", "<cmd>BufferLineMoveNext<cr>", desc = "Move Buffer Right" },
-    { "[B", vim.NIL },
-    { "]B", vim.NIL },
-  },
+	"akinsho/bufferline.nvim",
+	opts = function(_, opts)
+		local bufferline = require("bufferline")
+		vim.tbl_deep_extend("force", opts, {
+			options = {
+				style_preset = {
+					bufferline.style_preset_no_bold,
+					bufferline.style_preset_no_italic,
+				},
+			},
+		})
+	end,
 }
