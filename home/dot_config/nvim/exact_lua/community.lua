2456d5afff6b47a212200260c26d62a288b113d6
Magnus Reyes <me@magnusreyes.com>
2024-07-18 13:09:09 -0400

    Change goto definition to jump on single result

--- home/dot_config/nvim/exact_lua/community.lua
+++ home/dot_config/nvim/exact_lua/community.lua
@@ -1,12 +1,31 @@
 ---@type LazySpec
 return {
-	"AstroNvim/astrocommunity",
-	{ import = "astrocommunity.editing-support.conform-nvim" },
-	{ import = "astrocommunity.pack.astro" },
-	{ import = "astrocommunity.pack.cpp" },
-	{ import = "astrocommunity.pack.lua" },
-	{ import = "astrocommunity.pack.svelte" },
-	{ import = "astrocommunity.pack.typescript" },
-	{ import = "astrocommunity.search.nvim-spectre" },
-	{ import = "astrocommunity.fuzzy-finder.fzf-lua" },
+  "AstroNvim/astrocommunity",
+  { import = "astrocommunity.editing-support.conform-nvim" },
+  { import = "astrocommunity.pack.astro" },
+  { import = "astrocommunity.pack.cpp" },
+  { import = "astrocommunity.pack.lua" },
+  { import = "astrocommunity.pack.svelte" },
+  { import = "astrocommunity.pack.typescript" },
+  { import = "astrocommunity.search.nvim-spectre" },
+  {
+    import = "astrocommunity.fuzzy-finder.fzf-lua",
+  },
+  {
+    ---@module "astrolsp"
+    "AstroNvim/astrolsp",
+    ---@type AstroLSPOpts
+    opts = {
+      mappings = {
+        n = {
+          gd = {
+            function()
+              require("fzf-lua").lsp_definitions({ jump_to_single_result = true })
+            end,
+            desc = "Go to symbol definition",
+          },
+        },
+      },
+    },
+  },
 }
