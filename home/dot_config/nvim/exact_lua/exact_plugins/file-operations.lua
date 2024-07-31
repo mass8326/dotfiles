d6b4294c0dcb8c3139c930e05843466c5b5fda16
Magnus Reyes <me@magnusreyes.com>
2024-07-31 08:52:45 -0400

    Add lsp aware file operations to nvim

---
+++ home/dot_config/nvim/exact_lua/exact_plugins/file-operations.lua
@@ -0,0 +1,9 @@
+return {
+	"antosha417/nvim-lsp-file-operations",
+	-- optional = true,
+	event = "VeryLazy",
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+		"nvim-neo-tree/neo-tree.nvim",
+	},
+}
