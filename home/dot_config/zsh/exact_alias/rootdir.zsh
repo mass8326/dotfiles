8303ee619307f9d3b1669dd40f1cc8c179e80475
Magnus Reyes <me@magnusreyes.com>
2024-08-15 10:30:42 -0400

    Add cd function for rootdir util

--- home/dot_config/zsh/exact_alias/rootdir.zsh
+++ home/dot_config/zsh/exact_alias/rootdir.zsh
@@ -1,23 +1,20 @@
 alias rt='cd $(git rev-parse --show-toplevel)'
 
+function rtc() {
+  _rt_get_package ./*/*
+  local dir=$(dirname $package)
+  cmd="cd $dir"
+  print -s $cmd
+  eval $cmd
+}
+
 function rtr() {
   if [[ $+commands[jq] != 1 ]]; then
     echo "Error: Could not find executable 'jq'"
     return 1
   fi
 
-  local packages=$(find . -type d \( -name node_modules -o -path '*/.*' \) -prune -o -name package.json -print)
-  if [[ -z $packages ]]; then
-    echo "Error: There's no 'package.json'"
-    return 1
-  else
-    local package=$(echo $packages | fzf --prompt="package.json location 󰄾 " --height ~50% --layout=reverse --border --tac --select-1)
-  fi
-
-  if [[ -z $package ]]; then
-    echo "Exit: You didn't select a package.json file"
-    return 1
-  fi
+  _rt_get_package .
   
   local scripts=$(cat $package | jq .scripts | sed '1d;$d' | fzf --prompt="$package script 󰄾 " --height ~50% --layout=reverse --border)
   if [[ -z $scripts ]]; then
@@ -34,4 +31,19 @@ function rtr() {
   fi
   print -s $cmd
   eval $cmd
+}
+
+function _rt_get_package() {
+  local packages=$(find $@ -type d \( -name node_modules -o -path '*/.*' \) -prune -o -name package.json -print)
+  if [[ -z $packages ]]; then
+    echo "Error: There's no child 'package.json'"
+    return 1
+  else
+    package=$(echo $packages | fzf --prompt="package.json location 󰄾 " --height ~50% --layout=reverse --border --tac --select-1)
+  fi
+
+  if [[ -z $package ]]; then
+    echo "Exit: You didn't select a package.json file"
+    return 1
+  fi
 }
