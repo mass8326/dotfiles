f814df353866f299b314ba94119c81bd608cf7b3
Magnus Reyes <me@magnusreyes.com>
2024-08-22 14:23:59 -0400

    Update mini surround which-key

--- home/dot_config/nvim/exact_lua/exact_plugins/mini.lua
+++ home/dot_config/nvim/exact_lua/exact_plugins/mini.lua
@@ -4,154 +4,173 @@
 -- For "a", it will include the non-whitespace line surrounding the indent block.
 -- "a" is line-wise, "i" is character-wise.
 local function ai_indent(ai_type)
-	local spaces = (" "):rep(vim.o.tabstop)
-	local lines = vim.api.nvim_buf_get_lines(0, 0, -1, false)
-	local indents = {} ---@type {line: number, indent: number, text: string}[]
+  local spaces = (" "):rep(vim.o.tabstop)
+  local lines = vim.api.nvim_buf_get_lines(0, 0, -1, false)
+  local indents = {} ---@type {line: number, indent: number, text: string}[]
 
-	for l, line in ipairs(lines) do
-		if not line:find("^%s*$") then
-			indents[#indents + 1] = { line = l, indent = #line:gsub("\t", spaces):match("^%s*"), text = line }
-		end
-	end
+  for l, line in ipairs(lines) do
+    if not line:find("^%s*$") then
+      indents[#indents + 1] = { line = l, indent = #line:gsub("\t", spaces):match("^%s*"), text = line }
+    end
+  end
 
-	local ret = {} ---@type (Mini.ai.region | {indent: number})[]
+  local ret = {} ---@type (Mini.ai.region | {indent: number})[]
 
-	for i = 1, #indents do
-		if i == 1 or indents[i - 1].indent < indents[i].indent then
-			local from, to = i, i
-			for j = i + 1, #indents do
-				if indents[j].indent < indents[i].indent then
-					break
-				end
-				to = j
-			end
-			from = ai_type == "a" and from > 1 and from - 1 or from
-			to = ai_type == "a" and to < #indents and to + 1 or to
-			ret[#ret + 1] = {
-				indent = indents[i].indent,
-				from = { line = indents[from].line, col = ai_type == "a" and 1 or indents[from].indent + 1 },
-				to = { line = indents[to].line, col = #indents[to].text },
-			}
-		end
-	end
+  for i = 1, #indents do
+    if i == 1 or indents[i - 1].indent < indents[i].indent then
+      local from, to = i, i
+      for j = i + 1, #indents do
+        if indents[j].indent < indents[i].indent then
+          break
+        end
+        to = j
+      end
+      from = ai_type == "a" and from > 1 and from - 1 or from
+      to = ai_type == "a" and to < #indents and to + 1 or to
+      ret[#ret + 1] = {
+        indent = indents[i].indent,
+        from = { line = indents[from].line, col = ai_type == "a" and 1 or indents[from].indent + 1 },
+        to = { line = indents[to].line, col = #indents[to].text },
+      }
+    end
+  end
 
-	return ret
+  return ret
 end
 
 local function ai_buffer(ai_type)
-	local start_line, end_line = 1, vim.fn.line("$")
-	if ai_type == "i" then
-		-- Skip first and last blank lines for `i` textobject
-		local first_nonblank, last_nonblank = vim.fn.nextnonblank(start_line), vim.fn.prevnonblank(end_line)
-		-- Do nothing for buffer with all blanks
-		if first_nonblank == 0 or last_nonblank == 0 then
-			return { from = { line = start_line, col = 1 } }
-		end
-		start_line, end_line = first_nonblank, last_nonblank
-	end
+  local start_line, end_line = 1, vim.fn.line("$")
+  if ai_type == "i" then
+    -- Skip first and last blank lines for `i` textobject
+    local first_nonblank, last_nonblank = vim.fn.nextnonblank(start_line), vim.fn.prevnonblank(end_line)
+    -- Do nothing for buffer with all blanks
+    if first_nonblank == 0 or last_nonblank == 0 then
+      return { from = { line = start_line, col = 1 } }
+    end
+    start_line, end_line = first_nonblank, last_nonblank
+  end
 
-	local to_col = math.max(vim.fn.getline(end_line):len(), 1)
-	return { from = { line = start_line, col = 1 }, to = { line = end_line, col = to_col } }
+  local to_col = math.max(vim.fn.getline(end_line):len(), 1)
+  return { from = { line = start_line, col = 1 }, to = { line = end_line, col = to_col } }
 end
 
-local function ai_whichkey()
-	---@type table<string, string|table>
-	local i = {
-		[" "] = "Whitespace",
-		['"'] = 'Balanced "',
-		["'"] = "Balanced '",
-		["`"] = "Balanced `",
-		["("] = "Balanced (",
-		[")"] = "Balanced ) including white-space",
-		[">"] = "Balanced > including white-space",
-		["<lt>"] = "Balanced <",
-		["]"] = "Balanced ] including white-space",
-		["["] = "Balanced [",
-		["}"] = "Balanced } including white-space",
-		["{"] = "Balanced {",
-		["?"] = "User Prompt",
-		_ = "Underscore",
-		a = "Argument",
-		b = "Balanced ), ], }",
-		c = "Class",
-		d = "Digit(s)",
-		e = "Word in CamelCase & snake_case",
-		f = "Function",
-		g = "Entire file",
-		i = "Indent",
-		o = "Block, conditional, loop",
-		q = "Quote `, \", '",
-		t = "Tag",
-		u = "Use/call function & method",
-		U = "Use/call without dot in name",
-	}
-	local a = vim.deepcopy(i)
-	for k, v in pairs(a) do
-		a[k] = v:gsub(" including.*", "")
-	end
+-- https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/util/mini.lua
+local function ai_whichkey(opts)
+  local objects = {
+    { " ", desc = "whitespace" },
+    { '"', desc = '" string' },
+    { "'", desc = "' string" },
+    { "(", desc = "() block" },
+    { ")", desc = "() block with ws" },
+    { "<", desc = "<> block" },
+    { ">", desc = "<> block with ws" },
+    { "?", desc = "user prompt" },
+    { "U", desc = "use/call without dot" },
+    { "[", desc = "[] block" },
+    { "]", desc = "[] block with ws" },
+    { "_", desc = "underscore" },
+    { "`", desc = "` string" },
+    { "a", desc = "argument" },
+    { "b", desc = ")]} block" },
+    { "c", desc = "class" },
+    { "d", desc = "digit(s)" },
+    { "e", desc = "CamelCase / snake_case" },
+    { "f", desc = "function" },
+    { "g", desc = "entire file" },
+    { "i", desc = "indent" },
+    { "o", desc = "block, conditional, loop" },
+    { "q", desc = "quote `\"'" },
+    { "t", desc = "tag" },
+    { "u", desc = "use/call" },
+    { "{", desc = "{} block" },
+    { "}", desc = "{} with ws" },
+  }
 
-	local ic = vim.deepcopy(i)
-	local ac = vim.deepcopy(a)
-	for key, name in pairs({ n = "Next", l = "Last" }) do
-		i[key] = vim.tbl_extend("force", { name = "Inside " .. name .. " textobject" }, ic)
-		a[key] = vim.tbl_extend("force", { name = "Around " .. name .. " textobject" }, ac)
-	end
-	require("which-key").register({
-		mode = { "o", "x" },
-		i = i,
-		a = a,
-	})
+  local ret = { mode = { "o", "x" } }
+  ---@type table<string, string>
+  local mappings = vim.tbl_extend("force", {}, {
+    around = "a",
+    inside = "i",
+    around_next = "an",
+    inside_next = "in",
+    around_last = "al",
+    inside_last = "il",
+  }, opts.mappings or {})
+  mappings.goto_left = nil
+  mappings.goto_right = nil
+
+  for name, prefix in pairs(mappings) do
+    name = name:gsub("^around_", ""):gsub("^inside_", "")
+    ret[#ret + 1] = { prefix, group = name }
+    for _, obj in ipairs(objects) do
+      local desc = obj.desc
+      if prefix:sub(1, 1) == "i" then
+        desc = desc:gsub(" with ws", "")
+      end
+      ret[#ret + 1] = { prefix .. obj[1], desc = obj.desc }
+    end
+  end
+  require("which-key").add(ret, { notify = false })
 end
 
 ---@type LazySpec
 return {
-	{
-		"echasnovski/mini.ai",
-		version = "*",
-		opts = function()
-			local ai = require("mini.ai")
-			vim.schedule(ai_whichkey)
-			return {
-				n_lines = 500,
-				custom_textobjects = {
-					o = ai.gen_spec.treesitter({ -- code block
-						a = { "@block.outer", "@conditional.outer", "@loop.outer" },
-						i = { "@block.inner", "@conditional.inner", "@loop.inner" },
-					}),
-					f = ai.gen_spec.treesitter({ a = "@function.outer", i = "@function.inner" }), -- function
-					c = ai.gen_spec.treesitter({ a = "@class.outer", i = "@class.inner" }), -- class
-					t = { "<([%p%w]-)%f[^<%w][^<>]->.-</%1>", "^<.->().*()</[^/]->$" }, -- tags
-					d = { "%f[%d]%d+" }, -- digits
-					e = { -- Word with case
-						{
-							"%u[%l%d]+%f[^%l%d]",
-							"%f[%S][%l%d]+%f[^%l%d]",
-							"%f[%P][%l%d]+%f[^%l%d]",
-							"^[%l%d]+%f[^%l%d]",
-						},
-						"^().*()$",
-					},
-					i = ai_indent,
-					g = ai_buffer,
-					u = ai.gen_spec.function_call(), -- u for "Usage"
-					U = ai.gen_spec.function_call({ name_pattern = "[%w_]" }), -- without dot in function name
-				},
-			}
-		end,
-	},
-	{
-		"echasnovski/mini.surround",
-		version = "*",
-		opts = {
-			mappings = {
-				add = "gsa", -- Add surrounding in Normal and Visual modes
-				delete = "gsd", -- Delete surrounding
-				find = "gsf", -- Find surrounding (to the right)
-				find_left = "gsF", -- Find surrounding (to the left)
-				highlight = "gsh", -- Highlight surrounding
-				replace = "gsr", -- Replace surrounding
-				update_n_lines = "gsn", -- Update `n_lines`
-			},
-		},
-	},
+  -- https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/plugins/coding.lua
+  {
+    "echasnovski/mini.ai",
+    version = "*",
+    opts = function(_, opts)
+      local ai = require("mini.ai")
+      vim.schedule(function()
+        ai_whichkey(opts)
+      end)
+      return {
+        n_lines = 500,
+        custom_textobjects = {
+          o = ai.gen_spec.treesitter({
+            a = { "@block.outer", "@conditional.outer", "@loop.outer" },
+            i = { "@block.inner", "@conditional.inner", "@loop.inner" },
+          }),
+          f = ai.gen_spec.treesitter({ a = "@function.outer", i = "@function.inner" }),
+          c = ai.gen_spec.treesitter({ a = "@class.outer", i = "@class.inner" }),
+          t = { "<([%p%w]-)%f[^<%w][^<>]->.-</%1>", "^<.->().*()</[^/]->$" },
+          d = { "%f[%d]%d+" },
+          e = {
+            {
+              "%u[%l%d]+%f[^%l%d]",
+              "%f[%S][%l%d]+%f[^%l%d]",
+              "%f[%P][%l%d]+%f[^%l%d]",
+              "^[%l%d]+%f[^%l%d]",
+            },
+            "^().*()$",
+          },
+          i = ai_indent,
+          g = ai_buffer,
+          u = ai.gen_spec.function_call(),
+          U = ai.gen_spec.function_call({ name_pattern = "[%w_]" }),
+        },
+      }
+    end,
+    config = function(_, opts)
+      require("mini.ai").setup(opts)
+      vim.schedule(function()
+        ai_whichkey(opts)
+      end)
+    end,
+  },
+  {
+    "echasnovski/mini.surround",
+    version = "*",
+    opts = {
+      mappings = {
+        add = "gsa", -- Add surrounding in Normal and Visual modes
+        delete = "gsd", -- Delete surrounding
+        find = "gsf", -- Find surrounding (to the right)
+        find_left = "gsF", -- Find surrounding (to the left)
+        highlight = "gsh", -- Highlight surrounding
+        replace = "gsr", -- Replace surrounding
+        update_n_lines = "gsn", -- Update `n_lines`
+      },
+    },
+  },
 }
