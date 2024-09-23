031c852df94309bf60bcd5faae4b66d87acd6ef1
Magnus Reyes <me@magnusreyes.com>
2024-09-23 09:22:29 -0400

    Change and rename functions for rooter

---
+++ home/dot_config/zsh/exact_alias/rooter.zsh
@@ -0,0 +1,67 @@
+alias rt='cd $(git rev-parse --show-toplevel)'
+
+# Global variables for passing between functions:
+# $_rt_package
+# $_rt_script
+
+function rtc() {
+  _rtfn_get_package ./*/ || return 1
+  local dir=$(dirname $package)
+  local cmd="cd $dir"
+  print -s $cmd
+  eval $cmd
+}
+
+function rts() {
+  if [[ -f "package.json" ]]; then
+    _rt_package="package.json"
+  else
+    _rtfn_get_package . || return 1
+  fi
+
+  _rtfn_get_script $_rt_package || return 1
+
+  local dir=$(dirname $_rt_package)
+  if [[ $dir = "." ]]; then
+    local cmd="pnpm run $_rt_script"
+  else
+    local cmd="(cd $dir && pnpm run $_rt_script)"
+  fi
+  print -s $cmd
+  eval $cmd
+}
+
+function _rtfn_get_script() {
+  if [[ $+commands[jq] != 1 ]]; then
+    echo "Error: Could not find executable 'jq'"
+    return 1
+  fi
+
+  local raw=$(cat $1 | jq .scripts | sed '1d;$d' | fzf --prompt="$1 script 󰄾 " --height ~50% --layout=reverse --border)
+  if [[ -z $raw ]]; then
+    echo "Exit: You didn't select a package.json script"
+    return 1
+  fi
+
+  _rt_script=$(echo $raw | awk -F ': ' '{gsub(/[" ]/, "", $1); print $1}')
+}
+
+function _rtfn_get_package() {
+  if [[ $+commands[fd] != 1 ]]; then
+    echo "Error: Could not find executable 'fd'"
+    return 1
+  fi
+
+  local packages=$(fd package.json $@ | sort | awk '{print gsub("/", "/") " " $0}' | sort -nr | cut -d' ' -f2-)
+  if [[ -z $packages ]]; then
+    echo "Error: There's no child 'package.json'"
+    return 1
+  else
+    _rt_package=$(echo $packages | fzf --prompt="package.json location 󰄾 " --height ~50% --layout=reverse --border --tac --select-1)
+  fi
+
+  if [[ -z $package ]]; then
+    echo "Exit: You didn't select a package.json file"
+    return 1
+  fi
+}
