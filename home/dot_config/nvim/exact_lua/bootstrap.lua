e4a8a3290a7e0854f3d3c123c9991dfcc981f51f
Magnus Reyes <me@magnusreyes.com>
2024-10-09 21:24:16 -0400

    Change nvim plugins to depend on vscode presence

--- home/dot_config/nvim/exact_lua/bootstrap.lua
+++ home/dot_config/nvim/exact_lua/bootstrap.lua
@@ -1,40 +1,59 @@
 ---@type LazySpec
 local spec = {
-	{
-		---@module "AstroNvim/AstroNvim"
-		"AstroNvim/AstroNvim",
-		version = "^4",
-		import = "astronvim.plugins",
-		---@type AstroNvimOpts
-		opts = {
-			mapleader = " ",
-			maplocalleader = ",",
-			icons_enabled = true,
-			pin_plugins = nil,
-			update_notifications = true,
-		},
-	},
-	{ import = "community" },
-	{ import = "ui" },
-	{ import = "langs" },
-	{ import = "plugins" },
+  {
+    ---@module "AstroNvim/AstroNvim"
+    "AstroNvim/AstroNvim",
+    version = "^4",
+    import = "astronvim.plugins",
+    ---@type AstroNvimOpts
+    opts = {
+      mapleader = " ",
+      maplocalleader = ",",
+      icons_enabled = true,
+      pin_plugins = nil,
+      update_notifications = true,
+    },
+  },
 }
+
+---@type LazySpecImport[]
+local native = {
+  { import = "community" },
+  { import = "ui" },
+  { import = "langs" },
+  { import = "plugins" },
+}
+
+---@type LazySpecImport[]
+local vscode = {
+  { import = "code" },
+}
+
+if vim.g.vscode then
+  for _, v in ipairs(vscode) do
+    table.insert(spec, v)
+  end
+else
+  for _, v in ipairs(native) do
+    table.insert(spec, v)
+  end
+end
 
 ---@type LazyConfig
 local config = {
-	install = { colorscheme = { "astrodark" } },
-	ui = { backdrop = 100 },
-	performance = {
-		rtp = {
-			disabled_plugins = {
-				"gzip",
-				"netrwPlugin",
-				"tarPlugin",
-				"tohtml",
-				"zipPlugin",
-			},
-		},
-	},
+  install = { colorscheme = { "astrodark" } },
+  ui = { backdrop = 100 },
+  performance = {
+    rtp = {
+      disabled_plugins = {
+        "gzip",
+        "netrwPlugin",
+        "tarPlugin",
+        "tohtml",
+        "zipPlugin",
+      },
+    },
+  },
 }
 
 require("lazy").setup(spec, config)
