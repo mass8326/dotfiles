954a137ac7826317fa84d9c69547afa937e70df3
Magnus Reyes <me@magnusreyes.com>
2024-10-11 15:55:14 -0400

    Update font config for wezterm

--- home/dot_wezterm.lua
+++ home/dot_wezterm.lua
@@ -5,7 +5,12 @@ local config = wezterm.config_builder()
 
 config.use_resize_increments = true
 
-config.font = wezterm.font_with_fallback({ "Inconsolata Nerd Font" })
+config.font_size = 11
+config.font = wezterm.font_with_fallback({
+  "CommitMono Nerd Font",
+  "Hack Nerd Font",
+  { family = "Inconsolata Nerd Font", scale = 1.2 },
+})
 
 config.inactive_pane_hsb = {
   saturation = 0.90,
