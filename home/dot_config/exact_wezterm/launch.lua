4539b66bc74a7bbb7d3f215881efbf0cafbab0f3
Magnus Reyes <me@magnusreyes.com>
2025-03-19 16:43:07 -0400

    Add doskey macro file for aliases in windows cmd

--- home/dot_config/exact_wezterm/launch.lua
+++ home/dot_config/exact_wezterm/launch.lua
@@ -1,4 +1,3 @@
-local wezterm = require("wezterm")
 local utils = require("utils")
 
 local config = {
@@ -23,18 +22,8 @@ local config = {
 if utils.is_linux() then
   config.default_prog = { "/usr/bin/zsh" }
 elseif utils.is_windows() then
-  config.launch_menu = { { args = { "cmd.exe" }, domain = { DomainName = "local" } } }
-  local wsl_domains = wezterm.default_wsl_domains()
-  for _, dom in ipairs(wsl_domains) do
-    if dom.name == "WSL:Ubuntu-20.04" then
-      config.default_domain = "WSL:Ubuntu-20.04"
-      dom.default_cwd = "~"
-    end
-    if dom.name == "WSL:Ubuntu" then
-      config.default_domain = "WSL:Ubuntu"
-      dom.default_cwd = "~"
-    end
-  end
+  config.default_prog = { "cmd.exe", "/K", "doskey", "/macrofile=%localappdata%/doskey/macro.txt" }
+  config.launch_menu = { { args = config.default_prog, domain = { DomainName = "local" } } }
 end
 
 return config
