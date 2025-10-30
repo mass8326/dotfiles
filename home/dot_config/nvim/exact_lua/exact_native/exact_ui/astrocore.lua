3ea862b2c07e9eb371b36fb627b646b3f3174bcf
Magnus Reyes <me@reyma.dev>
2025-10-30 10:44:29 -0400

    Clean up smart splits nvim configuration

--- home/dot_config/nvim/exact_lua/exact_native/exact_ui/astrocore.lua
+++ home/dot_config/nvim/exact_lua/exact_native/exact_ui/astrocore.lua
@@ -1,3 +1,5 @@
+---@module "astrocore"
+
 ---@type LazySpec
 return {
   "AstroNvim/astrocore",
@@ -28,23 +30,12 @@ return {
         signcolumn = "yes",
         wrap = false,
       },
-      g = {
-      },
     },
     mappings = {
       n = {
         -- Movement
         ["<C-d>"] = { "<C-d>zz", desc = "Scroll Down" },
         ["<C-u>"] = { "<C-u>zz", desc = "Scroll Up" },
-        -- Smart splits
-        ["<C-Left>"] = { require("smart-splits").move_cursor_left, desc = "Smart split move left" },
-        ["<C-Down>"] = { require("smart-splits").move_cursor_down, desc = "Smart split move down" },
-        ["<C-Up>"] = { require("smart-splits").move_cursor_up, desc = "Smart split move up" },
-        ["<C-Right>"] = { require("smart-splits").move_cursor_right, desc = "Smart split move right" },
-        ["<A-Left>"] = { require("smart-splits").resize_left, desc = "Smart split resize left" },
-        ["<A-Down>"] = { require("smart-splits").resize_down, desc = "Smart split resize down" },
-        ["<A-Up>"] = { require("smart-splits").resize_up, desc = "Smart split resize up" },
-        ["<A-Right>"] = { require("smart-splits").resize_right, desc = "Smart split resize right" },
         -- Buffer nav
         ["<S-Left>"] = { function() require("astrocore.buffer").nav(-vim.v.count1) end, desc = "Prev buffer" },
         ["<S-Right>"] = { function() require("astrocore.buffer").nav(vim.v.count1) end, desc = "Next buffer" },
--- home/dot_config/nvim/exact_lua/exact_native/exact_ui/splits.lua
+++ home/dot_config/nvim/exact_lua/exact_native/exact_ui/splits.lua
@@ -1,6 +1,26 @@
+---@module "astrocore"
+
 ---@type LazySpec
 return {
   "mrjones2014/smart-splits.nvim",
   version = ">=1.0.0",
   lazy = false,
+  specs = {
+    "AstroNvim/astrocore",
+    ---@type AstroCoreOpts
+    opts = {
+      mappings = {
+        n = {
+          ["<C-Left>"] = { require("smart-splits").move_cursor_left, desc = "Smart split move left" },
+          ["<C-Down>"] = { require("smart-splits").move_cursor_down, desc = "Smart split move down" },
+          ["<C-Up>"] = { require("smart-splits").move_cursor_up, desc = "Smart split move up" },
+          ["<C-Right>"] = { require("smart-splits").move_cursor_right, desc = "Smart split move right" },
+          ["<A-Left>"] = { require("smart-splits").resize_left, desc = "Smart split resize left" },
+          ["<A-Down>"] = { require("smart-splits").resize_down, desc = "Smart split resize down" },
+          ["<A-Up>"] = { require("smart-splits").resize_up, desc = "Smart split resize up" },
+          ["<A-Right>"] = { require("smart-splits").resize_right, desc = "Smart split resize right" },
+        },
+      },
+    },
+  },
 }
