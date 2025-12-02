e79b001664b9f2e0826d28a237b9ead1bec3c4a3
Magnus Reyes <me@reyma.dev>
2025-12-02 13:10:54 -0500

    Remove smart-splits due to issues with tmux

--- home/dot_config/nvim/exact_lua/exact_native/exact_ui/bindings.lua
+++ home/dot_config/nvim/exact_lua/exact_native/exact_ui/bindings.lua
@@ -1,27 +1,26 @@
 ---@type LazySpec
 return {
-  "AstroNvim/astrocore",
-  ---@type AstroCoreOpts
-  opts = {
-    mappings = {
-      n = {
-        -- Movement
-        ["<C-d>"] = { "<C-d>zz", desc = "Scroll Down" },
-        ["<C-u>"] = { "<C-u>zz", desc = "Scroll Up" },
-        -- Buffer Nav
-        ["<S-Left>"] = { function() require("astrocore.buffer").nav(-vim.v.count1) end, desc = "Prev buffer" },
-        ["<S-Right>"] = { function() require("astrocore.buffer").nav(vim.v.count1) end, desc = "Next buffer" },
-        ["]b"] = { function() require("astrocore.buffer").move(-vim.v.count1) end, desc = "Move buffer left" },
-        ["[b"] = { function() require("astrocore.buffer").move(vim.v.count1) end, desc = "Move buffer right" },
-        -- Splits Nav
-        ["<C-Left>"] = { require("smart-splits").move_cursor_left, desc = "Smart split move left" },
-        ["<C-Down>"] = { require("smart-splits").move_cursor_down, desc = "Smart split move down" },
-        ["<C-Up>"] = { require("smart-splits").move_cursor_up, desc = "Smart split move up" },
-        ["<C-Right>"] = { require("smart-splits").move_cursor_right, desc = "Smart split move right" },
-        ["<A-Left>"] = { require("smart-splits").resize_left, desc = "Smart split resize left" },
-        ["<A-Down>"] = { require("smart-splits").resize_down, desc = "Smart split resize down" },
-        ["<A-Up>"] = { require("smart-splits").resize_up, desc = "Smart split resize up" },
-        ["<A-Right>"] = { require("smart-splits").resize_right, desc = "Smart split resize right" },
+  { "mrjones2014/smart-splits.nvim", enabled = false },
+  {
+    "AstroNvim/astrocore",
+    ---@type AstroCoreOpts
+    opts = {
+      mappings = {
+        n = {
+          -- Movement
+          ["<C-d>"] = { "<C-d>zz", desc = "Scroll Down" },
+          ["<C-u>"] = { "<C-u>zz", desc = "Scroll Up" },
+          -- Buffer Nav
+          ["<S-Left>"] = { function() require("astrocore.buffer").nav(-vim.v.count1) end, desc = "Move buffer left" },
+          ["<S-Right>"] = { function() require("astrocore.buffer").nav(vim.v.count1) end, desc = "Move buffer right" },
+          ["]b"] = { function() require("astrocore.buffer").move(-vim.v.count1) end, desc = "Move buffer left" },
+          ["[b"] = { function() require("astrocore.buffer").move(vim.v.count1) end, desc = "Move buffer right" },
+          -- Splits Nav
+          ["<C-Left>"] = { "<C-w>h", desc = "Move to left split" },
+          ["<C-Down>"] = { "<C-w>j", desc = "Move to below split" },
+          ["<C-Up>"] = { "<C-w>k", desc = "Move to above split" },
+          ["<C-Right>"] = { "<C-w>l", desc = "Move to right split" },
+        },
       },
     },
   },
