9ebf8364f1729a4bfe9592932f1604914d713cb5
Magnus Reyes <me@reyma.dev>
2026-01-08 10:45:03 -0500

    Add nh to nix home-manager

--- home/dot_config/home-manager/home.nix
+++ home/dot_config/home-manager/home.nix
@@ -24,5 +24,10 @@
     home-manager = {
       enable = true;
     };
+    nh = {
+      enable = true;
+      clean.enable = true;
+      clean.extraArgs = "--keep-since 4d --keep 3";
+    };
   };
 }
