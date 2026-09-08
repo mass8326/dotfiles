1ec242bbc7041ad3419499e1fd15ddc83f9cc142
Magnus Reyes <me@reyma.dev>
2026-09-08 17:49:22 -0400

    Change matugen to be optional for nvim theme

--- home/dot_config/nvim/lua/exact_native/exact_ui/noctalia.lua
+++ home/dot_config/nvim/lua/exact_native/exact_ui/noctalia.lua
@@ -1,6 +1,9 @@
 return {
   "RRethy/base16-nvim",
   config = function()
-    require("matugen").setup()
+    local success, matugen = pcall(require, "matugen")
+    if success then
+      matugen.setup()
+    end
   end,
 }
